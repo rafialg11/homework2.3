a469a55eb1b62d65e2156c7aa6fb1b6b3060c433
Rafi Alghifari <raffialg11@gmail.com>
2023-07-17 15:39:46 +0700

    Create dbConfig, Model, and MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require("express");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
-const PORT = 3000; 
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+const db = require('./models/')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log(`Database connected!`)
+    }).catch((err) => {
+        console.log(`Cannot connect to DB`, err)
+        process.exit()
+    });
 
 app.listen(PORT, () => { 
     console.log(`API is listening on port ${PORT}`); 
---
+++ models/song.js
@@ -0,0 +1,17 @@
+module.exports = (mongoose) => {
+    const schema = mongoose.Schema(
+        {
+            title: String,
+            artists: Array,
+            url: String 
+        }
+    )
+    schema.method("toJSON", function(){
+        const {__v, _id, ...object} = this.toObject()
+        object.id = _id
+        return object
+    })
+
+    const Song = mongoose.model("songs", schema)
+    return Song
+}
